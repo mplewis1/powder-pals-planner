3569095f6b62a1a83905b1db92c9063df16afef9
Matt Lewis <matthewpaullewis@gmail.com>
2025-08-17 18:11:50 -0400

    Add proper MIME type headers for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -26,14 +26,8 @@ export default defineConfig(({ mode }) => ({
     sourcemap: false,
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
-            return `assets/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
       },
